8173fd41ab4a1314d99308f89c3397207c0866e4
Leo Hong <hongliyu2002@hotmail.com>
2023-03-24 14:53:57 +0800

    Update

---
+++ src/Fluxera.Extensions.Hosting.Modules.Orleans.Streaming/Configuration/StreamingOptions.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+
+namespace Fluxera.Extensions.Hosting.Modules.Orleans.Streaming;
+
+/// <summary>
+/// </summary>
+[PublicAPI]
+public sealed class StreamingOptions
+{
+    /// <summary>
+    ///     Is this configuration intended for client-side use?
+    /// </summary>
+    public bool UsedByClient { get; set; }
+
+}
---
+++ src/Fluxera.Extensions.Hosting.Modules.Orleans.Streaming/ServiceCollectionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Fluxera.Extensions.Hosting.Modules.Orleans.Streaming;
+
+/// <summary>
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="options"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddOrleansStreaming(this IServiceCollection services, StreamingOptions options)
+    {
+        if (options.UsedByClient)
+        {
+            return services.AddOrleansClient(clientBuilder =>
+                                             {
+                                                 clientBuilder.AddStreaming();
+                                             });
+        }
+        return services.AddOrleans(siloBuilder =>
+                                   {
+                                       // siloBuilder.Configure<StreamingOptions>(streamingOptions =>
+                                       //                                         {
+                                       //                                             streamingOptions.MinimumStreamingPeriod = options.MinimumStreamingPeriod;
+                                       //                                             streamingOptions.RefreshStreamingListPeriod = options.RefreshStreamingListPeriod;
+                                       //                                             streamingOptions.InitializationTimeout = options.InitializationTimeout;
+                                       //                                         });
+                                       siloBuilder.AddStreaming();
+                                   });
+    }
+}
