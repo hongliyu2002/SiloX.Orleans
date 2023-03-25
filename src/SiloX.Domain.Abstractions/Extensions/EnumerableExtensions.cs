abea85a1eedfbb7397bf91463455e14532480cc3
Leo Hong <hongliyu2002@hotmail.com>
2023-03-26 07:43:11 +0800

    Update

---
+++ src/SiloX.Domain.Abstractions/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Immutable;
+using Orleans.FluentResults;
+
+namespace SiloX.Domain.Abstractions.Extensions;
+
+/// <summary>
+///     Provides extension methods for <see cref="IEnumerable{T}" /> objects.
+/// </summary>
+public static class EnumerableExtensions
+{
+    /// <summary>
+    ///     Converts a collection of <see cref="IError" /> objects into a concatenated string of error messages.
+    /// </summary>
+    /// <param name="errors">The collection of <see cref="IError" /> objects to convert.</param>
+    /// <returns>A concatenated string of error messages.</returns>
+    public static string ToReason(this IEnumerable<IError> errors)
+    {
+        return string.Join(';', errors.OfType<Error>().Select(error => error.ToString()));
+    }
+
+    /// <summary>
+    ///     Converts a collection of <see cref="IError" /> objects into an immutable list of error messages.
+    /// </summary>
+    /// <param name="errors">The collection of <see cref="IError" /> objects to convert.</param>
+    /// <returns>An immutable list of error messages.</returns>
+    public static IImmutableList<string> ToReasons(this IEnumerable<IError> errors)
+    {
+        return errors.OfType<Error>().Select(error => error.ToString()).ToImmutableList();
+    }
+
+    /// <summary>
+    ///     Converts a collection of key-value pairs into a string representing the sorting criteria for a query.
+    /// </summary>
+    /// <param name="sortings">The collection of key-value pairs representing the sorting criteria.</param>
+    /// <returns>A string representing the sorting criteria for a query.</returns>
+    public static string ToSortStrinng(this IEnumerable<KeyValuePair<string, bool>> sortings)
+    {
+        return string.Join(',', sortings.Select(x => $"{x.Key}{(x.Value ? " DESC" : "")}"));
+    }
+}
---
+++ src/SiloX.Domain.Abstractions/Extensions/QueryableExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Immutable;
+
+namespace SiloX.Domain.Abstractions.Extensions;
+
+/// <summary>
+///     Provides extension methods for <see cref="IQueryable{T}" />.
+/// </summary>
+public static class QueryableExtensions
+{
+    /// <summary>
+    ///     Asynchronously converts an <see cref="IQueryable{T}" /> sequence to an immutable list.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements in the sequence.</typeparam>
+    /// <param name="queryable">The <see cref="IQueryable{T}" /> to convert to an immutable list.</param>
+    /// <param name="cancellationToken">The cancellation token that can be used to cancel the operation.</param>
+    /// <returns>An immutable list that contains the elements of the input sequence.</returns>
+    public static async Task<ImmutableList<TSource>> ToImmutableListAsync<TSource>(this IQueryable<TSource> queryable, CancellationToken cancellationToken = default)
+    {
+        var result = ImmutableList.CreateBuilder<TSource>();
+        await foreach (var element in queryable.ToAsyncEnumerable().WithCancellation(cancellationToken))
+        {
+            result.Add(element);
+        }
+        return result.ToImmutable();
+    }
+}
