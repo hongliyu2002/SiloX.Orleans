dd18e439e24dbd850cfe8b7221ab3e7df03d7cab
Leo Hong <hongliyu2002@hotmail.com>
2023-03-29 21:23:15 +0800

    Update

--- samples/Vending.Domain.Abstractions/Events/SnackMachines/SnackMachineEvent.cs
+++ samples/Vending.Domain.Abstractions/Events/SnackMachines/SnackMachineEvent.cs
@@ -6,12 +6,4 @@ namespace Vending.Domain.Abstractions.Events;
 [Serializable]
 [GenerateSerializer]
 public abstract record SnackMachineEvent(Guid MachineId, int Version, Guid TraceId, DateTimeOffset OperatedAt, string OperatedBy) 
-    : DomainEvent(MachineId.ToString(), Version, TraceId, OperatedAt, OperatedBy)
-{
-    private readonly Guid _machineId = MachineId;
-    public Guid MachineId
-    {
-        get => _machineId;
-        init => _machineId = value;
-    }
-}
+    : DomainEvent(MachineId.ToString(), Version, TraceId, OperatedAt, OperatedBy);
--- samples/Vending.Domain.Abstractions/Events/Snacks/SnackEvent.cs
+++ samples/Vending.Domain.Abstractions/Events/Snacks/SnackEvent.cs
@@ -5,13 +5,5 @@ namespace Vending.Domain.Abstractions.Events;
 [Immutable]
 [Serializable]
 [GenerateSerializer]
-public abstract record SnackEvent(Guid SnackId, int Version, Guid TraceId, DateTimeOffset OperatedAt, string OperatedBy)
-    : DomainEvent(SnackId.ToString(), Version, TraceId, OperatedAt, OperatedBy)
-{
-    private readonly Guid _snackId = SnackId;
-    public Guid SnackId
-    {
-        get => _snackId;
-        init => _snackId = value;
-    }
-}
+public abstract record SnackEvent(Guid SnackId, int Version, Guid TraceId, DateTimeOffset OperatedAt, string OperatedBy) 
+    : DomainEvent(SnackId.ToString(), Version, TraceId, OperatedAt, OperatedBy);
