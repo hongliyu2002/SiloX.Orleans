bb41d8a10d0b12739855171d195ddefee845e232
Leo Hong <hongliyu2002@hotmail.com>
2023-04-14 05:04:32 +0800

    Update

--- samples/Vending.BlazorApp/Contributors/ConfigureBlazorAppOptionsContributor.cs
+++
@@ -1,9 +0,0 @@
-﻿using Fluxera.Extensions.Hosting.Modules.Configuration;
-
-namespace Vending.BlazorApp.Contributors;
-
-internal sealed class ConfigureBlazorAppOptionsContributor : ConfigureOptionsContributorBase<BlazorAppOptions>
-{
-    /// <inheritdoc />
-    public override string SectionName => "Vending:BlazorApp";
-}
